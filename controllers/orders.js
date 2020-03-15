d4c374721c1b784ce762ef3705e5f12fcf31eb19
Sergey Zelenov <mail@zelenov.su>
2020-03-15 20:07:26 +0100

    send order confirmation email

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,6 +1,8 @@
 const Order = require('../models/Order');
 const Product = require('../models/Product');
 const mapOrder = require('../mappers/order');
+const sendMail = require('../libs/sendMail');
+const config = require('../config');
 
 module.exports.checkout = async function checkout(ctx, next) {
   const product = await Product.findOne({slug: ctx.request.body.product});
@@ -10,6 +12,16 @@ module.exports.checkout = async function checkout(ctx, next) {
     product: product,
     phone: ctx.request.body.phone,
     address: ctx.request.body.address,
+  });
+  
+  await sendMail({
+    to: {
+      name: ctx.user.displayName,
+      address: ctx.user.email,
+    },
+    subject: 'Подтверждение заказа',
+    locals: {href: config.host, product: product.title, price: `${product.price}₽`},
+    template: 'order',
   });
 
   ctx.body = {order: order.id};
