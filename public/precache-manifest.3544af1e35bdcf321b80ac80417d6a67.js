18c041c7ac5e936abdb1cf5cc157ad7bd68889cb
Sergey Zelenov <mail@zelenov.su>
2020-03-15 11:11:06 +0100

    client

---
+++ public/precache-manifest.3544af1e35bdcf321b80ac80417d6a67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafc6f19d034829002bd8281280cfa74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4bf958125edc9a0b43e",
+    "url": "/static/js/2.7eb25e73.chunk.js"
+  },
+  {
+    "revision": "3724165f573246efa0fc9e96fed5b4e2",
+    "url": "/static/js/2.7eb25e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4a2dfe3cc205b05107",
+    "url": "/static/js/main.f8844a6c.chunk.js"
+  },
+  {
+    "revision": "3334d444db2f9a712615",
+    "url": "/static/js/runtime-main.49272589.js"
+  }
+]);
